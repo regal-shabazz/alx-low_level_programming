a30a3a5f326098477dc84651eefb4291cd4aef11
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-16 02:41:30 -0800

    5-printf.c1.0.0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  * main - Entry point
  *
  * printf: This function display the specified string in the output
- * return: Always 0 (success)
+ * Return: Returns 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
