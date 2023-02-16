e32ddd6cc4b1a3df08d734ddeea2c0ae8970c078
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-16 02:23:24 -0800

    4-puts.c1.0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
