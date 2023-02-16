d460b5e2fc70872175214feb94f5a78c57b097cf
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-16 02:38:30 -0800

    5-printf.c1.0.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf: This function display the specified string in the output
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
