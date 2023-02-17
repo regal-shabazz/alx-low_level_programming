72dd64e51d83366b4bc64ecaf6e88cc9cd80e5b5
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-17 12:30:07 -0800

    print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char tebahpla= 'z';
+
+	while (tebahpla >= 'a')
+	{
+		putchar(tebahpla);
+		tebahpla++;
+	}
+	putchar('\n');
+
+	return (0);
+}
