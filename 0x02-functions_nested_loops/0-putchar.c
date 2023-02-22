bfee5d001e441763d6f6fbcb1c08a4b7e0be6d4b
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-21 16:04:51 -0800

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int _putchar(char c);
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
