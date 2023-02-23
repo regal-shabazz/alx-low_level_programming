4ca840987cb7020ab8680da45c5ed7d6d96f914c
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-23 05:57:52 -0800

    multiple mains: main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Debugging.
+ * main - Debugging
  *
  * Return: 0
  */
@@ -12,5 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
+	return 0;
 }
