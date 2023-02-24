b1fa60dbf4103f05cad38f3756ae7f101c5ae7e4
Babatunde Oshin <orsheenxi@gmail.com>
2023-02-23 19:18:48 -0800

    program multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
+ * mul - multiplies two integers.
  * @a: int to be multiplied to b
  * @b: int to be multiplied to a
  * Return: the result of the operation
